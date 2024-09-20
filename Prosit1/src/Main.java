3154818835ca698c26d9db0a0cd26aa6f2f6db49
durangorobin <jeaclaude2023@gmail.com>
2024-09-20 14:32:13 +0100

    sauvegarde du Prosit1 version 1.0

--- Prosit1/src/Main.java
+++ Prosit1/src/Main.java
@@ -1,18 +1,43 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
     public static void main(String[] args) {
+        Double prixI;
+        Scanner sc= new  Scanner(System.in);
         // nous allons créer un poduit vide c'est à dire sans initialisation nommé p0
         Produit p0=new Produit();
         // créons trois nouveaux produits avec les paramètres. nommé p1,p2 et p3
-        Produit p1 = new Produit(1021,"lait","delice",0.0);
-        Produit p2 = new Produit(2510,"Yaourt","Vitalait",0.0);
+        Produit p1 = new Produit(1021,"lait","delice");
+        Produit p2 = new Produit(2510,"Yaourt","Vitalait");
         Produit p3 = new Produit(3250,"Tomate","Sicam",1.200);
         p1.afficher();
         p2.afficher();
         p3.afficher();
-        p1.setPrix(0.700);
+        p1.prix=0.700;
         p1.afficher();
-        }
+        // complètons les informations manquantes pour chaque produit
+        System.out.println("Entrez un prix pour complèter le produit de marque Lait");
+        prixI=sc.nextDouble();
+        p1.prix=prixI;
+        System.out.println("voici l'affichage du produit lait après avoir complèter l'information manquante");
+        p1.afficher();
+        System.out.println("Entrez un prix pour complèter le produit de marque Yaourt");
+        prixI=sc.nextDouble();
+        p2.prix=prixI;
+        System.out.println("voici l'affichage du produit Yaourt après avoir complèter l'information manquante");
+        p2.afficher();
+        System.out.println("Entrez un prix pour complèter le produit de marque Tomate");
+        prixI=sc.nextDouble();
+        p3.prix=prixI;
+        System.out.println("voici l'affichage du produit Tomate après avoir complèter l'information manquante");
+        p3.afficher();
+        //7. Afficher les produits déjà crées à l’aide de la méthode toString().
+        System.out.println("voici l'affichage du produit à l'aide de la methode toString");
+        System.out.println(p1.toString());
+
     }
+}
+
--- Prosit1/src/Produit.java
+++ Prosit1/src/Produit.java
@@ -1,18 +1,17 @@
+import java.util.Date;
+
 public class Produit {
-    private    int identifiant;
+    protected     int identifiant;
     private String libelle;
     private String marque;
-    private Double prix;
-
+    Double prix;
+    private Date dateExpiration;
 
     public Produit(){
-        System.out.println("création d'un nouveau produit vide");
-        identifiant=0;
-        libelle=" ";
-        marque=" ";
-        prix=0.0;
 
     }
+
+
     public Produit(int p_identifiant,String p_marque,String p_libelle,Double p_prix){
         System.out.println("création d'un nouveau produit avec les données");
         identifiant=p_identifiant;
@@ -20,22 +19,38 @@ public class Produit {
         marque=p_marque;
         prix=p_prix;
     }
-    public int getIdentifiant(){return this.identifiant;  }
-    public void setIdentifiant(int p_identifiant){this.identifiant=p_identifiant;}
-    public String getLibelle(){ return this.libelle;}
-    public void  setLibelle(String p_libelle){this.libelle=p_libelle;}
-    public String getMarque(){ return this.marque;}
-    public void setMarque(String marque) {
+
+    public Produit(int identifiant, String libelle, String marque) {
+        this.identifiant = identifiant;
+        this.libelle = libelle;
         this.marque = marque;
     }
-    public Double getPrix(){ return this.prix;}
-    public void setPrix(Double p_prix){this.prix=p_prix;}
+
+    public Produit(int identifiant, String libelle, String marque, Double prix, Date dateExpiration) {
+        this.identifiant = identifiant;
+        this.libelle = libelle;
+        this.marque = marque;
+        this.prix = prix;
+        this.dateExpiration = dateExpiration;
+    }
 
     public void afficher(){
-        System.out.println("l'identifiant du produit est "+getIdentifiant());
-        System.out.println("Le libellé du produit est "+getLibelle());
-        System.out.println("La marque du produit est "+getMarque());
-        System.out.println("Le Prx du  produit est "+getPrix());
+        System.out.println("l'identifiant du produit est "+this.identifiant);
+        System.out.println("Le libellé du produit est "+this.libelle);
+        System.out.println("La marque du produit est "+this.marque);
+        System.out.println("Le Prx du  produit est "+this.prix);
 
     }
-}
+
+    @Override
+    public String toString() {
+        return "Produit{" +
+                "identifiant=" + identifiant +
+                ", libelle='" + libelle + '\'' +
+                ", marque='" + marque + '\'' +
+                ", prix=" + prix +
+                ", dateExpiration=" + dateExpiration +
+                '}';
+    }
+
+}git
