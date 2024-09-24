4d1ea414a2017aff52de52de48a1ef8584555195
durangorobin <jeaclaude2023@gmail.com>
2024-09-24 13:24:07 +0100

    version corrigé du prosit2

--- Prosit2/src/Produit.java
+++ Prosit2/src/Produit.java
@@ -1,10 +1,12 @@
+import java.util.Scanner;
+
 public class Produit {
     protected String nom;
     protected   Double prix;
     protected int nbreProduit;
 
     public Produit() {
-        this.nbreProduit=0;
+       // this.nbreProduit=0;
 
     }
 
@@ -30,5 +32,15 @@ public class Produit {
         } else if (prix<=0) { System.out.println("LE PRIX DOIT ETRE POSITIF ET NON NULLE");
 
         }
+    }
+    public void lireProdit(){
+        System.out.println(" LIRE LE NOM DU PRODUIT");
+        String name;
+        Double montprix;
+        Scanner nameP= new Scanner(System.in);
+        name=nameP.nextLine();
+        montprix=nameP.nextDouble();
+        this.nom=name;
+        this.prix=montprix;
     }
 }
