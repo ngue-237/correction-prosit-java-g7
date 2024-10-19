79d26a7b251f739c07097b7ab4e759218ffcf758
DJUISSI_Andreah <djuissiandreah@gmail.com>
2024-10-19 15:59:11 +0100

    sauvegarde 1

---
+++ Documents/formation-java/gestion_stock_g7/gestion_stock_g7/src/main/java/com/logonedigital/gestion_stock_g7/GestionStockG7Application.java
@@ -0,0 +1,13 @@
+package com.logonedigital.gestion_stock_g7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionStockG7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionStockG7Application.class, args);
+	}
+
+}
---
+++ Documents/formation-java/gestion_stock_g7/gestion_stock_g7/src/test/java/com/logonedigital/gestion_stock_g7/GestionStockG7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logonedigital.gestion_stock_g7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionStockG7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
