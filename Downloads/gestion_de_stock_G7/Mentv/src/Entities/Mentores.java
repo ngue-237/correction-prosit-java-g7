303b21473859862b44dc911d96070994a86d4216
DJUISSI_Andreah <djuissiandreah@gmail.com>
2024-12-17 19:13:04 +0100

    sauvegarde 1

---
+++ Downloads/gestion_de_stock_G7/Mentv/src/Entities/Mentores.java
@@ -0,0 +1,10 @@
+package Entities;
+
+public class Mentores {
+    private Integer MentoreId;
+    private String Nom;
+    private String prenom;
+    private String email;
+    private String 
+
+}
